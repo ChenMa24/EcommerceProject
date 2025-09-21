7fce4705dc9522a75a8f42b5a3839f37291b3d1f
Chen Ma <165631620+ChenMa24@users.noreply.github.com>
2025-09-21 15:26:29 +1000

    create checkout page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
-import { HomePage } from './pages/HomePage'
 import { Routes, Route } from 'react-router'
+import { HomePage } from './pages/HomePage'
+import { CheckoutPage } from './pages/CheckoutPage'
 
 function App() {
 
   return (
     <Routes>
       <Route path='/' element={<HomePage />} />
-      <Route path="checkout" element={<div>Checkout Page</div>} />
+      <Route path="checkout" element={<CheckoutPage />} />
     </Routes>
   )
 }
