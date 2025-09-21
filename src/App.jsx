c715fb85db0d48cfd379d629359cbedd9998ca03
Chen Ma <165631620+ChenMa24@users.noreply.github.com>
2025-09-21 15:05:36 +1000

    create multiply pages using react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
 import { HomePage } from './pages/HomePage'
+import { Routes, Route } from 'react-router'
 
 function App() {
 
   return (
-    <HomePage />
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path="checkout" element={<div>Checkout Page</div>} />
+    </Routes>
   )
 }
 
